1197e7011be7ff951b860eb69e5e8cec53cf8d5c
Pavel Karpenko <pavelkv@gmail.com>
2023-04-11 21:45:18 +0300

    feat: implement task - simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -6,24 +6,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 const chainMaker = {
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    if (!this.chain) {
+      this.chain = [];
+    }
+
+    this.chain.push(`( ${value === null ? value : value.toString()} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (typeof (position) !== "number"
+        || position !== parseInt(position)
+        || position <= 0 
+        || position > this.chain.length)  {
+      this.chain = null;
+      throw new Error("You can't remove incorrect link!");
+    }
+
+    if (this.chain) {
+      this.chain.splice(position - 1, 1);
+    }
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.chain) {
+      this.chain.reverse();
+    }
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const stringChain = this.chain.join("~~");
+    this.chain = null;
+    return stringChain;
   }
 };
 
