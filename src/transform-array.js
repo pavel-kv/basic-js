ebde34abbac6e263eda74fc8cfed94861d46caaa
Pavel Karpenko <pavelkv@gmail.com>
2023-04-11 20:00:00 +0300

    feat: implement task - transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  const result = [];
+
+  for (let i = 0; i < arr.length; i += 1) {
+    switch (arr[i]) {
+      case "--discard-next":
+        if (arr[i + 1]) {
+          i += 1;
+        }
+        break;
+      case "--discard-prev":
+        if (i && arr[i - 1] === result[result.length - 1]) {
+          result.pop();
+        }
+        break;
+      case "--double-next":
+        if (arr[i + 1]) {
+          result.push(arr[i + 1]);
+        }
+        break;
+      case "--double-prev":
+        if (i && arr[i - 1] === result[result.length - 1]) {
+          result.push(arr[i - 1]);
+        }
+        break;
+      default:
+        result.push(arr[i]);
+        break;
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
