0e37e34447d57378be550734513ce28c36841960
Pavel Karpenko <pavelkv@gmail.com>
2023-04-13 18:36:41 +0300

    fear: implement task - vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,68 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(typeMachine = true) {
+    this.isDirectMachine = typeMachine;
+    this.codeFirstSymbol = "A".codePointAt(0);
+    this.codeLastSymbol = "Z".codePointAt(0);
+    this.mod = this.codeLastSymbol - this.codeFirstSymbol + 1;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    const messageUpperCase = message.toUpperCase();
+    const keyWord = key.toUpperCase();
+    const keyString = keyWord.padEnd(messageUpperCase.length, keyWord);
+
+    let index = 0;
+    const encryptedMessage = [];
+
+    for (let symbol of messageUpperCase) {
+      const codeSymbol = symbol.codePointAt(0);
+
+      if (this.isLatinAlpabetSymbol(symbol)) {
+        encryptedMessage.push(String.fromCodePoint(((codeSymbol + keyString.codePointAt(index)) % this.mod) + this.codeFirstSymbol));
+        index += 1;
+      } else {
+        encryptedMessage.push(symbol);
+      }
+    }
+
+    return this.isDirectMachine ? encryptedMessage.join("") : encryptedMessage.reverse().join("");
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    const encryptedMessageUpperCase = encryptedMessage.toUpperCase();
+    const keyWord = key.toUpperCase();
+    const keyString = keyWord.padEnd(encryptedMessageUpperCase.length, keyWord);
+
+    let index = 0
+    const decryptedMessage = [];
+
+    for (let symbol of encryptedMessageUpperCase) {
+      const codeSymbol = symbol.codePointAt(0);
+
+      if (this.isLatinAlpabetSymbol(symbol)) {
+        decryptedMessage.push(String.fromCodePoint(((codeSymbol + this.mod - keyString.codePointAt(index)) % this.mod) + this.codeFirstSymbol));
+        index += 1;
+      } else {
+        decryptedMessage.push(symbol);
+      }
+    }
+
+    return this.isDirectMachine ? decryptedMessage.join("") : decryptedMessage.reverse().join("");
+  }
+
+  isLatinAlpabetSymbol(symbol) {
+    const codeSymbol = symbol.codePointAt(0);
+    return (codeSymbol >= this.codeFirstSymbol) && (codeSymbol <= this.codeLastSymbol);
   }
 }
 
